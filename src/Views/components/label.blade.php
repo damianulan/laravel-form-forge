29e530fc862e61e8ff594cf82c2a7268be08b65d
Damian Ułan <damian.ulan@protonmail.com>
2025-10-05 16:26:37 +0200

    Label component key 2

--- src/Views/components/label.blade.php
+++ src/Views/components/label.blade.php
@@ -1,6 +1,6 @@
 <label for="id_{{ $name }}" class="col-form-label formforge-label{{ $required ? ' required':'' }}">
     {{ $label }}
     @if($key)
-        <div class="text-muted formforge-key">{{ $key }}</div>
+        <div class="text-muted formforge-key">{{ '[' . $key . ']' }}</div>
     @endif
 </label>
