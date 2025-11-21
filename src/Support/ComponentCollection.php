a44f41a4d6b1b2aa847cccb8b1e880c55eb2da0a
damianulan <31820473+damianulan@users.noreply.github.com>
2025-11-21 19:52:24 +0000

    Apply automatic changes

--- src/Support/ComponentCollection.php
+++ src/Support/ComponentCollection.php
@@ -58,7 +58,7 @@ class ComponentCollection extends Collection
      */
     public function getComponents(): self
     {
-        return $this->filter(fn($component) => $component instanceof ForgeComponent);
+        return $this->filter(fn ($component) => $component instanceof ForgeComponent);
     }
 
     /**
@@ -79,7 +79,7 @@ class ComponentCollection extends Collection
 
     private function checkInputType($input): void
     {
-        if (! ($input instanceof ForgeComponent || $input instanceof ForgeSection)) {
+        if ( ! ($input instanceof ForgeComponent || $input instanceof ForgeSection)) {
             throw new ForgeCollectionIllegalInput();
         }
     }
