0f6a4d40ff06d860a6e6d9fbaac7779356176a23
Damian Ułan <damian.ulan@protonmail.com>
2025-11-21 20:54:26 +0100

    section gettitle fix

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,5 +1,5 @@
 <div class="formforge-section">
-    <h4 class="formforge-section__title">{{ $component->title }}</h4>
+    <h4 class="formforge-section__title">{{ $component->getTitle() }}</h4>
     <div class="formforge-section__content">
         @foreach($component->getComponents() as $component)
                 @include('formforge::templates.components.' . $template, ['component' => $component])
